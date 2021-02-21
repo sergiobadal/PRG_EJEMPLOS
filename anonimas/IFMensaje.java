5d86949d24e5eafc6df27772084201a3023b0845
Sergio Badal <6311025+sergiobadal@users.noreply.github.com>
2021-02-21 14:28:17 +0100

    Delete anonimas directory

--- anonimas/IFMensaje.java
+++
@@ -1,7 +0,0 @@
-package anonimas;
-@FunctionalInterface
-public interface IFMensaje {
-
-    public String imprime(String t);
-
-}
--- anonimas/MensajeImplementado.java
+++
@@ -1,7 +0,0 @@
-package anonimas;
-public class MensajeImplementado implements IFMensaje {
-    //@Override
-    public String imprime(String t) {
-        return t; // imprime 1 vez
-    }
-}
